84bd7a7b03c3151f929f81493c9b1b043a10168d
Beata-Krasnopolska <beata.krasnopolska@interia.pl>
2018-11-14 10:27:01 +0100

    Add project files.

---
+++ FrequencyOfNumberLINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrequencyOfNumberLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> digits = new List<int> { 2, 8, 1, 2, 9, 2, 5, 6, 7, 3, 7, 6, 8, 5, 4, 8, 9, 2, 3, 7 };
+
+            Console.WriteLine("Program displays the number and the frequency of numbers in a given list of numbers");
+
+            var number = from digit in digits
+                         group digit by digit into y
+                         select y;
+
+            foreach (var listNo in number)
+            {
+                Console.WriteLine("The number {0} appears {1} times", listNo.Key, listNo.Count());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
